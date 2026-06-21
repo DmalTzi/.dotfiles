6dd2bcb6a185f8418c0e3698d71cdc3050b1e613
pok <audyomg@gmail.com>
2026-06-21 14:53:40 +0700

    new config and lsp setup

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,22 +1,4 @@
--- vim interface setup
-local tab_size = 4
-vim.opt.number = true
-vim.opt.relativenumber = true
-vim.opt.cursorline = false
+require("config")
+require("keymaps").setup()
 
--- vim indent
-vim.opt.tabstop = tab_size
-vim.opt.shiftwidth = tab_size
 
--- vim font setup
-vim.opt.ambw = "double"
-
--- vim map toggle relativenumber
-vim.keymap.set('n', 'eN', function()
-	vim.opt.relativenumber = not vim.opt.relativenumber:get()
-end)
-
-local commenter = require("my_tools.commenter")
-
-vim.keymap.set({'n', 'i'}, '<C-/>', commenter.toggle_signle_comment, {silent = true, noremap = true})
-vim.keymap.set('v', '<C-/>', commenter.toggle_multiple_comment, {silent = true, noremap = true})
---
+++ nvim/lua/config.lua
@@ -0,0 +1,15 @@
+-- leader
+vim.g.mapleader = " "
+
+-- vim interface setup
+local tab_size = 4
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.cursorline = false
+
+-- vim indent
+vim.opt.tabstop = tab_size
+vim.opt.shiftwidth = tab_size
+
+-- vim font setup
+vim.opt.ambw = "double"
